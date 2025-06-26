884f06358a6329895845ab2553705ea6d3d2c5d6
RedRedRyan <ryankipkorir2002@gmail.com>
2025-06-27 01:24:03 +0300

    tail section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import gsap from 'gsap';
+import Cocktails from './components/Cocktails';
 
 
 
@@ -14,7 +15,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className='h-dvh bg-black'/>
+      <Cocktails />
     </main>
   ) 
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -94,11 +94,7 @@ const Hero = () => {
             </div>
 
             <div className="view-cocktails">
-              <p className="subtitle">
-                Every cocktail on our menu is a blend of premium ingredients,
-                creative flair, and timeless recipes — designed to delight your
-                senses.
-              </p>
+              <p className="subtitle">Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses</p>
               <a href="#cocktails">View cocktails</a>
             </div>
           </div>
